631f1d08bb25f70804a767abe545fdc9f19b0308
pushpender <115529172+Pushpender0@users.noreply.github.com>
2026-08-24 12:01:11 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+string encode(vector<string> &strs)
+{
+    string ans = "";
+    for (string str : strs)
+    {
+        ans += to_string(str.size()) + "#" + str;
+    }
+    return ans;
+}
+
+vector<string> decode(string s)
+{
+
+    vector<string> ans;
+    int n = s.length();
+    int i = 0;
+    while (i < n)
+    {
+        int j = i;
+        // while (j < n && s[j] != '#')
+        //     j++;
+
+        // if (j == n)
+        //     break;
+        while (s[j] != '#')
+        {
+            j++;
+        }
+
+        int len = stoi(s.substr(i, j - i));
+        ans.push_back(s.substr(j + 1, len));
+        i = j + 1 + len;
+    }
+
+    return ans;
+}
+};
